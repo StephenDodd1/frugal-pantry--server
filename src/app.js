204b63bb6735056c4ef026518e8e6ee0a870dcd4
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-10-27 13:34:17 -0500

    created and tested initial endpoint to recieve user data on signup

--- src/app.js
+++ src/app.js
@@ -3,18 +3,38 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const winston = require("winston");
+const jsonBodyParser = express.json();
 
 const app = express();
-
-app.get("/api/*", (req,res) => {
-  res.json({ok: true})
-})
+app.post("/api/users/signup", jsonBodyParser, (req, res) => {
+  const user = {
+    firstName,
+    lastName,
+    email,
+    phone,
+    companyName,
+    companyType,
+    address,
+    postal,
+    city,
+    country,
+    state,
+    userName,
+    password,
+    repeatPassword,
+  } = req.body;
+  let fn = user.firstName
+  res.status(200).json({user: `User is ${fn}`})
+});
+app.get("/api/*", (req, res) => {
+  res.json({ ok: true });
+});
 app.get("*", function (req, res, next) {
   if (req.url === "/") return next();
 });
-app.get('/', function (req,res,next) {
-  res.send('Hello World!')
-})
-app.listen(8000,() => {
-  console.log('Server is running!')
-})
+app.get("/", function (req, res, next) {
+  res.send("Hello World!");
+});
+app.listen(8000, () => {
+  console.log("Server is running!");
+});
